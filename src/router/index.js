34a193558f112f91faebe17f5327811ce0cde16f
Suhyeon Kim <wisemuji@gmail.com>
2019-07-18 15:09:38 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ const router = new Router();
 
 router.get('/', (ctx) => ctx.body = '안녕하세요, 김수현입니다.');
 router.get('/deploy', (ctx) => ctx.body = '무중단배포 성공');
+router.get('/ha', (ctx) => ctx.body = '소스 코드 변경!');
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUser);
